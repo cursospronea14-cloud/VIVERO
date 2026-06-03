fe2d7c6c1e74321bb37da56555ea936d617281a0
cursospronea14-cloud <cursospronea14@gmail.com>
2026-06-02 23:18:00 -0600

    Add new colors and font families to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'verde-oscuro': '#1B4332',
         'agave': '#2D6A4F',
-        'arena': '#E9D8A6',
         'flor': '#E76F51',
         'hueso': '#FDFBF7',
+        'piedra': '#4A4A4A',
+        'gris-info': '#F5F5F0',
+        'arena': '#E9D8A6',
         'gris-texto': '#2D2D2D',
         'gris-suave': '#6B6B6B',
         'exito': '#38A169',
         'peligro': '#C53A1F',
+      },
+      fontFamily: {
+        'serif': ['Georgia', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
       },
     },
   },
