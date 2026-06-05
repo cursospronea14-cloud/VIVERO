b8a357cb54c67b84d2e3c84b2d138f0b6fc84c85
cursospronea14-cloud <cursospronea14@gmail.com>
2026-06-05 15:31:08 -0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
               </div>
               <div>
                 <h1 className="font-bold text-xl">DESIERTO QUE FLORECE</h1>
-                <p className="text-xs text-[#E9D8A6]">Cactus y Suculentas</p>
+                <p className="text-xs text-[#E9D8A6]">Plantas Ornamentales, Cactus y Suculentas</p>
               </div>
             </Link>
 
