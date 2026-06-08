1ffb9aebf138e08a896130667a09708ad692d0f7
cursospronea14-cloud <cursospronea14@gmail.com>
2026-06-07 21:36:14 -0600

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,14 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// La URL es la misma
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-
-// Esta es la clave que copiaste de Supabase
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
-
-if (!supabaseUrl || !supabaseServiceKey) {
-  console.error('❌ Faltan variables de entorno para supabaseAdmin')
-}
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
   auth: {
